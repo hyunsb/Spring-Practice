fed858f1883476d435d28e35370cc0f44f22e01b
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-16 14:02:25 +0900

    feat: Update Welcome page HelloController

--- hello-spring/hello-spring/src/main/java/hyunsb/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hyunsb/hellospring/controller/HelloController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+    String name = "정현수";
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!!!");
+        model.addAttribute("data", name);
+
+        // templates/hello.html 을 랜더링한다.
+        // 이 때, model(date : hello!!!)
         return "hello";
     }
 }
