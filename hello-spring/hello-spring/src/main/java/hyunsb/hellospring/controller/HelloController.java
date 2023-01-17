8ca16b4f46a2080f60a3a3725ceec863f0a74a19
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-17 20:25:29 +0900

    feat: update hello-mvc Controller

--- hello-spring/hello-spring/src/main/java/hyunsb/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hyunsb/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hyunsb.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,12 @@ public class HelloController {
         // templates/hello.html 을 랜더링한다.
         // 이 때, model(date : hello!!!)
         return "hello";
+    }
+
+    //@RequestParam("가져올 데이터의 이름") [데이터 타입], [변수명]
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
