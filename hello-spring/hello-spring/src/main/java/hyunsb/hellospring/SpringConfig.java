58d3e9469a0c78008a6bb703388776ab192566e8
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-19 05:11:12 +0900

    feat: update Jdbc 회원 리포지토리

--- hello-spring/hello-spring/src/main/java/hyunsb/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hyunsb/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hyunsb.hellospring;
 
+import hyunsb.hellospring.repository.JdbcMemberRepository;
 import hyunsb.hellospring.repository.MemberRepository;
 import hyunsb.hellospring.repository.MemoryMemberRepository;
 import hyunsb.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+//    @Autowired DataSource dataSource;
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
