84b62d9c9fe9c6579c1d3385ef5a0d584efd285c
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-18 22:03:33 +0900

    feat: Update Member join service

--- hello-spring/hello-spring/src/main/java/hyunsb/hellospring/controller/MemberController.java
+++ hello-spring/hello-spring/src/main/java/hyunsb/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hyunsb.hellospring.controller;
 
+import hyunsb.hellospring.domain.Member;
 import hyunsb.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // 스프링 컨테이너에서 스프링 빈이 관리
 @Controller
@@ -14,5 +17,21 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
