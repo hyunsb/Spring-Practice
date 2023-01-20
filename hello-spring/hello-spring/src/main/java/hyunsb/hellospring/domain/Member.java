bc9ac75de37eff7a4087e29a6313459ef79f5b4d
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-20 17:47:21 +0900

    feat: Member management service - ADD JPA library

--- hello-spring/hello-spring/src/main/java/hyunsb/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hyunsb/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hyunsb.hellospring.domain;
 
+import javax.persistence.*;
+
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;        // 시스템이 저장하는 ID, 데이터 식별자
+
+//    @Column(name = "name")
     private String name;    // 회원 이름
 
     public String getName() {
