30f817958ccbae3fad9ac09444f923badaa8b27f
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-21 03:38:58 +0900

    feat: UserManagement Service

---
+++ UserManagement/UserManagement/src/test/java/hyunsb/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunsb.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
