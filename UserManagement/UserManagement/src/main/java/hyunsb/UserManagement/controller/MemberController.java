371806ced79e944b96a5d0a4112ac1af279c3854
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-22 20:28:00 +0900

    feat(User Management Service): update MemberController, new.html

---
+++ UserManagement/UserManagement/src/main/java/hyunsb/UserManagement/controller/MemberController.java
@@ -0,0 +1,14 @@
+package hyunsb.UserManagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("/members/new")
+    public String join() {
+        return "members/new";
+    }
+}
