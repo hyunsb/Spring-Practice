a1a19f209fa472f29ce44477694e97ddc0518844
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-26 02:13:45 +0900

    feat(Board Service): Update boardList view

--- board/board/src/main/java/com/hyunsb/board/controller/BoardController.java
+++ board/board/src/main/java/com/hyunsb/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.hyunsb.board.entity.Board;
 import com.hyunsb.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,8 +27,8 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(){
-
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());
         return "boardlist";
     }
 }
--- board/board/src/main/java/com/hyunsb/board/service/BoardService.java
+++ board/board/src/main/java/com/hyunsb/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.hyunsb.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -14,4 +16,7 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    public List<Board> boardList(){
+        return boardRepository.findAll();
+    }
 }
