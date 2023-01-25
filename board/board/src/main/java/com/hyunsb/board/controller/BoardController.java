d723e2a3bb7b8cba0379b72b8bb49312437e066b
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-26 01:22:39 +0900

    feat(Board Service): Update build directory

--- board/board/src/main/java/com/hyunsb/board/controller/BoardController.java
+++ board/board/src/main/java/com/hyunsb/board/controller/BoardController.java
@@ -1,2 +1,16 @@
-package com.hyunsb.board.controller;public class BoardController {
+package com.hyunsb.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main(){
+        return "Hello World";
+    }
+
 }
