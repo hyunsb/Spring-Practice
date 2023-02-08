0a4b1cfdcd8214503349c52fbf403d01d1765a3d
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-08 17:17:17 +0900

    feat(brandMe): Update project

---
+++ brandMe/brandMe/src/main/java/com/hyunsb/brandMe/BrandMeApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsb.brandMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandMeApplication.class, args);
+	}
+
+}
---
+++ brandMe/brandMe/src/test/java/com/hyunsb/brandMe/BrandMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsb.brandMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
