df6086a32d6f425eb27e28c31b5915808cd523f5
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-08 21:50:49 +0900

    feat(brandMe): Update project

---
+++ brandme/brandme/src/main/java/com/hyunsb/brandme/BrandmeApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsb.brandme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandmeApplication.class, args);
+	}
+
+}
