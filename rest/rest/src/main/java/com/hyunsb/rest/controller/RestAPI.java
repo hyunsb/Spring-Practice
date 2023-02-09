144a286e2a7c40a4dce0e1b5d33eeff818a79aac
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-09 20:28:24 +0900

    feat(rest): Update domain book

--- rest/rest/src/main/java/com/hyunsb/rest/controller/RestAPI.java
+++ rest/rest/src/main/java/com/hyunsb/rest/controller/RestAPI.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class RestAPI {
 
-    @RequestMapping(method = RequestMethod.GET, path = "미정")
+    @RequestMapping(method = RequestMethod.GET, path = "/book")
     public void getAll(){
 
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "미정")
+    @RequestMapping(method = RequestMethod.POST, path = "/book")
     public void Add(){
 
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "미정")
+    @RequestMapping(method = RequestMethod.PUT, path = "/book/{id}")
     public void Update(){
 
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "미정")
+    @RequestMapping(method = RequestMethod.DELETE, path = "/book/{id}")
     public void Delete(){
 
     }
