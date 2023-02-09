4465fb74ea5d5c737449705126821983f712e96f
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-09 20:44:38 +0900

    feat(rest): Update Entity

--- rest/rest/src/main/java/com/hyunsb/rest/domain/Book.java
+++ rest/rest/src/main/java/com/hyunsb/rest/domain/Book.java
@@ -1,10 +1,20 @@
 package com.hyunsb.rest.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
+
     private String author;
+
     private int price;
 
     public int getId() {
---
+++ rest/rest/src/main/java/com/hyunsb/rest/repository/RestRepository.java
@@ -0,0 +1,8 @@
+package com.hyunsb.rest.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RestRepository {
+
+}
