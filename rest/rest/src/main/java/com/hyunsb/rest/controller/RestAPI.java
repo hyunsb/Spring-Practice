568bce7abcc0eb4a2d742241f755cf59d43c4b4a
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-09 20:35:33 +0900

    feat(rest): Update RestAPI Controller

--- rest/rest/src/main/java/com/hyunsb/rest/controller/RestAPI.java
+++ rest/rest/src/main/java/com/hyunsb/rest/controller/RestAPI.java
@@ -1,30 +1,37 @@
 package com.hyunsb.rest.controller;
 
+import com.hyunsb.rest.domain.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RestAPI {
 
+    /* 책 전체 조회 */
     @RequestMapping(method = RequestMethod.GET, path = "/book")
-    public void getAll(){
-
+    public String getAll(){
+        return "list";
     }
 
+    /* 책 정보 등록 */
     @RequestMapping(method = RequestMethod.POST, path = "/book")
-    public void Add(){
-
+    public String Add(@RequestBody Book book){
+        return "add";
     }
 
+    /* 책 정보 수정 */
     @RequestMapping(method = RequestMethod.PUT, path = "/book/{id}")
-    public void Update(){
-
+    public String Update(@RequestBody Book book, @PathVariable int id){
+        return "update";
     }
 
+    /* 책 정보 삭제 */
     @RequestMapping(method = RequestMethod.DELETE, path = "/book/{id}")
-    public void Delete(){
-
+    public String Delete(@PathVariable int id){
+        return "delete";
     }
 
 }
