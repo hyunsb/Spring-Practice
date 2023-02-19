433e09aa488f568de7e2c0ab4b329d1a49681d5a
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-20 00:53:08 +0900

    feat: Update product-order-service - 상품 등록 테스트코드 생성 POJO 방식

--- product-order-service/product-order-service/src/test/java/com/hyunsb/productorderservice/product/ProductServiceTest.java
+++ product-order-service/product-order-service/src/test/java/com/hyunsb/productorderservice/product/ProductServiceTest.java
@@ -4,19 +4,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductServiceTest {
 
     private ProductService productService;
+    private ProductPort productPort;
+    private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
-        productService = new ProductService();
+        productRepository = new ProductRepository();
+        productPort = new ProductAdapter(productRepository);
+        productService = new ProductService(productPort);
+        
     }
 
     @Test
     void 상품등록() {
         final String name = "상품명";
-        final Long price = 1000L;
+        final int price = 1000;
         DiscountPolicy discountPolicy = DiscountPolicy.NONE;
 
         final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
@@ -26,13 +34,21 @@ public class ProductServiceTest {
 
     private class ProductService {
 
+        private final ProductPort productPort;
+
+        private ProductService(ProductPort productPort) {
+            this.productPort = productPort;
+        }
+
         public void addProduct(AddProductRequest request) {
-            throw new UnsupportedOperationException("Unsupported addProduct");
+            final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+
+            productPort.save(product);
         }
     }
 
 
-    private record AddProductRequest(String name, Long price, DiscountPolicy discountPolicy) {
+    private record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
 
         public AddProductRequest{
 
@@ -47,4 +63,60 @@ public class ProductServiceTest {
         NONE
     }
 
+    private class Product {
+
+        private final String name;
+        private final int price;
+        private final DiscountPolicy discountPolicy;
+
+        private Long id;
+
+        public Product(String name, int price, DiscountPolicy discountPolicy) {
+            
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+
+            this.name = name;
+            this.price = price;
+            this.discountPolicy = discountPolicy;
+        }
+
+        public void assignId(final Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+    }
+
+    private interface ProductPort {
+        void save(final Product product);
+    }
+
+    private class ProductAdapter implements ProductPort {
+
+        private final ProductRepository productRepository;
+
+        private ProductAdapter(ProductRepository productRepository) {
+            this.productRepository = productRepository;
+        }
+
+        @Override
+        public void save(Product product) {
+            productRepository.save(product);
+        }
+    }
+
+    private class ProductRepository {
+
+        private Long sequence = 0L;
+        private Map<Long, Product> persistence = new HashMap<>();
+
+        public void save(Product product) {
+            product.assignId(++sequence);
+            persistence.put(product.getId(), product);
+        }
+    }
 }
