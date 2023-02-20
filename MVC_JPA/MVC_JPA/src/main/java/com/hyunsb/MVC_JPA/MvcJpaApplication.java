d5d9e27118d4ad48cc2955c44a84a16b9fef8f4c
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-21 00:27:24 +0900

    feat: MVC + JPA 학습 시작

---
+++ MVC_JPA/MVC_JPA/src/main/java/com/hyunsb/MVC_JPA/MvcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsb.MVC_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcJpaApplication.class, args);
+	}
+
+}
---
+++ MVC_JPA/MVC_JPA/src/test/java/com/hyunsb/MVC_JPA/MvcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsb.MVC_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
