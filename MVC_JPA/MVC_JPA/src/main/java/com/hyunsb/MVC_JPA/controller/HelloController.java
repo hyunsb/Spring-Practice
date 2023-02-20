ae2067e702b46b50ca45f91cf0a10307d891c6b9
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-21 02:11:54 +0900

    feat: Update ResponseBody, api 방식

--- MVC_JPA/MVC_JPA/src/main/java/com/hyunsb/MVC_JPA/controller/HelloController.java
+++ MVC_JPA/MVC_JPA/src/main/java/com/hyunsb/MVC_JPA/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.hyunsb.MVC_JPA.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,5 +24,38 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hyunsb!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body에 내용을 직접 입력, 데이터를 직접 응답
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    //json 방식으로 표현 {"key" : "value"}
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
