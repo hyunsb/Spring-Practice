800b4bb9c7aaa6b8c208f810e2246f93fcbf58ee
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-08 20:09:49 +0900

    feat: add Entity, type

---
+++ developer-Maker/src/main/java/com/example/developermaker/entity/Developer.java
@@ -0,0 +1,43 @@
+package com.example.developermaker.entity;
+
+import com.example.developermaker.type.DeveloperLevel;
+import com.example.developermaker.type.DeveloperSkillType;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperSkillType developerSkillType;
+
+    private Integer experienceYear;
+
+    private String memberId;
+
+    private String name;
+
+    private Integer age;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
