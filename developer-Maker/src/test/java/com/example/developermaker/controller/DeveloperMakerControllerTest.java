8c28add73e07804c79f6609599048e7e5132a0db
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-12 15:53:25 +0900

    test: Mock을 사용한 컨트롤러 Req, Res 테스트코드 작성

--- developer-Maker/src/test/java/com/example/developermaker/controller/DeveloperMakerControllerTest.java
+++ developer-Maker/src/test/java/com/example/developermaker/controller/DeveloperMakerControllerTest.java
@@ -1,14 +1,66 @@
 package com.example.developermaker.controller;
 
+import com.example.developermaker.dto.DeveloperDto;
+import com.example.developermaker.service.DeveloperMakerService;
+import com.example.developermaker.type.DeveloperLevel;
+import com.example.developermaker.type.DeveloperSkillType;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@MockBean(JpaMetamodelMappingContext.class)
 @WebMvcTest(DeveloperMakerController.class)
 class DeveloperMakerControllerTest {
 
+    private final MockMvc mockMvc;
+    protected MediaType contentType = new MediaType(
+            MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            StandardCharsets.UTF_8);
+    @MockBean
+    private DeveloperMakerService developerMakerService;
+
+    @Autowired
+    public DeveloperMakerControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
 
     @Test
-    void getAllDevelopers() {
+    void getAllDevelopers() throws Exception {
+        DeveloperDto developerDto1 = DeveloperDto.builder()
+                .developerSkillType(DeveloperSkillType.BACK_END)
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .memberId("memberId1").build();
+
+        DeveloperDto developerDto2 = DeveloperDto.builder()
+                .developerSkillType(DeveloperSkillType.FRONT_END)
+                .developerLevel(DeveloperLevel.SENIOR)
+                .memberId("memberId2").build();
+
+        BDDMockito.given(developerMakerService.getAllEmployedDevelopers())
+                .willReturn(Arrays.asList(developerDto1, developerDto2));
+
+        mockMvc.perform(get("/developers").contentType(contentType))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.[0].developerSkillType", CoreMatchers.is(DeveloperSkillType.BACK_END.name())))
+                .andExpect(jsonPath("$.[0].developerLevel", CoreMatchers.is(DeveloperLevel.JUNIOR.name())))
+                .andExpect(jsonPath("$.[1].developerSkillType", CoreMatchers.is(DeveloperSkillType.FRONT_END.name())))
+                .andExpect(jsonPath("$.[1].developerLevel", CoreMatchers.is(DeveloperLevel.SENIOR.name())));
     }
 
     @Test
