ff1299131c0552d43c8649db882e350291bbd0cc
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-12 16:18:17 +0900

    test: Mock을 사용한 컨트롤러 Req, Res 테스트코드 작성

--- developer-Maker/src/test/java/com/example/developermaker/controller/DeveloperMakerControllerTest.java
+++ developer-Maker/src/test/java/com/example/developermaker/controller/DeveloperMakerControllerTest.java
@@ -1,9 +1,11 @@
 package com.example.developermaker.controller;
 
+import com.example.developermaker.dto.DeveloperDetailDto;
 import com.example.developermaker.dto.DeveloperDto;
 import com.example.developermaker.service.DeveloperMakerService;
 import com.example.developermaker.type.DeveloperLevel;
 import com.example.developermaker.type.DeveloperSkillType;
+import com.example.developermaker.type.StatusCode;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -64,18 +66,25 @@ class DeveloperMakerControllerTest {
     }
 
     @Test
-    void getDeveloperDetail() {
-    }
+    void getDeveloperDetail() throws Exception {
+        DeveloperDetailDto developerDetail = DeveloperDetailDto.builder()
+                .developerSkillType(DeveloperSkillType.BACK_END)
+                .developerLevel(DeveloperLevel.SENIOR)
+                .memberId("memberId")
+                .experienceYear(12)
+                .name("홍길동")
+                .age(34)
+                .statusCode(StatusCode.EMPLOYED)
+                .build();
 
-    @Test
-    void editDeveloper() {
-    }
+        String testMemberId = "memberId";
+        BDDMockito.given(developerMakerService.getDeveloperDetail(testMemberId))
+                .willReturn(developerDetail);
 
-    @Test
-    void createDevelopers() {
-    }
-
-    @Test
-    void deleteDeveloper() {
+        mockMvc.perform(get("/developer/" + testMemberId).contentType(contentType))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.developerSkillType", CoreMatchers.is(DeveloperSkillType.BACK_END.name())))
+                .andExpect(jsonPath("$.developerLevel", CoreMatchers.is(DeveloperLevel.SENIOR.name())));
     }
 }
