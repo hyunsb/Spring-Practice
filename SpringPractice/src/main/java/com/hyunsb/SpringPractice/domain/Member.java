97a799418fb35a9c7e724bb7c0731be8597d427c
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-14 15:59:33 +0900

    김영한 Spring 강의 복습: 자바 로컬 레포지토리 기능 생성

---
+++ SpringPractice/src/main/java/com/hyunsb/SpringPractice/domain/Member.java
@@ -0,0 +1,14 @@
+package com.hyunsb.SpringPractice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Member {
+
+    private Long id;
+    private String name;
+}
---
+++ SpringPractice/src/main/java/com/hyunsb/SpringPractice/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package com.hyunsb.SpringPractice.repository;
+
+import com.hyunsb.SpringPractice.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = store.get(id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
