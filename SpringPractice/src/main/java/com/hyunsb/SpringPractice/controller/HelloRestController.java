13ca1612fde35baa2af64d159f01980c50f90abb
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-14 15:26:34 +0900

    김영한 Spring 강의 복습: Controller, RestController Mvc 사용해보기

--- SpringPractice/src/main/java/com/hyunsb/SpringPractice/controller/HelloRestController.java
+++ SpringPractice/src/main/java/com/hyunsb/SpringPractice/controller/HelloRestController.java
@@ -2,7 +2,9 @@ package com.hyunsb.SpringPractice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +13,13 @@ public class HelloRestController {
 
     @GetMapping("/helloRest")
     public ResponseEntity<?> hello() {
-        log.info("call helloRest");
+        log.info("helloRest");
         return ResponseEntity.ok("hello Rest!");
+    }
+
+    @GetMapping("helloRest-mvc")
+    public ResponseEntity<?> helloMvc(@RequestParam("name") String name) {
+        log.info("helloRest-Mvc");
+        return ResponseEntity.ok("안녕하세요, " + name + " 님");
     }
 }
