db7e2533d52a3ce5a0785501e88b3098b297e433
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-14 14:35:50 +0900

    김영한 Spring 강의 복습: 프로젝트 init

---
+++ SpringPractice/src/test/java/com/hyunsb/SpringPractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsb.SpringPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
